f0545fa384f1a692436370df3a4b4bc7ebd4da47
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-10 21:58:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,9 +17,9 @@ class Artist
     @@all
   end
 
-  def add_song(song)
-    Song.all.select {|song| song.artist == self}
-    @songs << song
-    song.artist = self
-  end
+  # def add_song(song)
+  #   Song.all.select {|song| song.artist == self}
+  #   @songs << song
+  #   song.artist = self
+  # end
 end
