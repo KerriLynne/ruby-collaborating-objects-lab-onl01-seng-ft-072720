a53859c036b466973457c844111cd6d0183bc761
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-10 22:53:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,16 @@ class Song
     @name = name
   end
 
-  def artist_name=(name)
+  def self.new_by_filename(file)
+   song_name = file.split(" - ")[1]
+   artist = file.split(" - ")[0]
+   song = self.new(song_name)
+   song.artist_name = artist
+   song
+ end 
+
+   artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
-  end  
+  end
 end
