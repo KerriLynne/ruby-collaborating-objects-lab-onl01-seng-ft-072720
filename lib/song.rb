6ea1532a7914c0e2b6c3e0bba19a6eb1783c9bfd
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-10 23:27:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,7 @@ class Song
    artist = file.split(" - ")[0]
    song = self.new(song_name)
    song.artist_name = artist
+   artist.add_song(song)
    song
   end
 
@@ -20,8 +21,6 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-
-
 
   def save
     @@all << self
