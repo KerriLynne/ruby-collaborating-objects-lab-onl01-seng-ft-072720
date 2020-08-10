0069034300e15f5622348a25127c92d52d0f2d25
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-10 22:47:40 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,4 +9,7 @@ class MP3Importer
      @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
   end  
 
+  def import
+    self.files.each {|file| Song.new_by_filename(file)}
+
 end
